82a03973c22b1ccf4d4c0513d15959feac718fac
Daria Svalova <svalova_d@skbkontur.ru>
2020-03-28 22:21:24 +0500

    add Lecture05 tasks

---
+++ src/Lecture05.hs
@@ -0,0 +1,108 @@
+module Lecture05 where
+
+{-
+  05: Ленивость
+
+  - Пример ленивых вычислений
+  - Ленивость в других языках (yield, ||, &&)
+  - Что такое ленивость
+    - thunk
+    - lazy evaluation
+    - whnf, hnf, nf
+  - Почему ленивость это хорошо
+    - бесконечные списки
+    - ленивость помогает компилятору оптимизировать код
+  - Почему ленивость это плохо
+    - space leaks
+  - Как с этим бороться
+    - (!, seq)
+    - foldl, foldl', foldr
+    - records strict fields
+    - LANGUAGE strict, LANGUAGE strictdata
+  - newtype lifting https://wiki.haskell.org/Newtype
+
+  Подробнее:
+    - https://www.fpcomplete.com/blog/2017/09/all-about-strictness
+    - https://wiki.haskell.org/Foldr_Foldl_Foldl'
+    - https://takenobu-hs.github.io/downloads/haskell_lazy_evaluation.pdf
+-}
+
+
+-- <Задачи для самостоятельного решения>
+{-
+    Напишите функцию, вычисляющую n-ое простое число с помощью
+    решета Эротосфена (https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes).
+    Используйте бесконечные списки.
+
+    Функция `sieve` "просеивает" список, переданный в качестве аргумента,
+    и возвращает другой список, но уже из простых чисел. Работает она так:
+    берёт очередное число из списка и выкидывает из него все числа, которые делятся на это число.
+
+    Чтобы с помощью `sieve` реализовать `nthPrime`, `sieve` должна работать на бесконечных списках.
+
+    https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes#/media/File:Sieve_of_Eratosthenes_animation.gif
+-}
+sieve :: [Integer] -> [Integer]
+sieve xs = error "not implemented"
+
+-- Функция, возвращающая n-ое простое число. Для её реализации используйте функцию sieve
+nthPrime :: Int -> Integer
+nthPrime n = error "not implemented"
+
+{-
+    Недавно в интервью Forbes с Сергеем Гуриевым Андрей Мовчан решил показать, что он
+    тоже в некотором смысле математик, но немного запутался в рассуждениях о ВВП Китая и США:
+
+        30 секунд с привязкой по времени: https://youtu.be/bTnnTeXHp8w?t=1782
+
+    Помогите математику Андрею как программисты. Используя бесконечные списки, напишите функцию,
+    которая вычислит через сколько лет ВВП на душу населения Китая догонит ВВП США.
+
+    ВВП Китая $10к на душу населения, растёт на 6% в год
+    ВВП США   $66к на душу населения, растёт на 2% в год.
+
+    Можете воспользоваться функцией [iterate](https://hoogle.haskell.org/?hoogle=iterate).
+    Она возвращает бесконечный список, каждый элемент которого получен из предыдущего
+    применением первого аргумента ко второму:
+
+        iterate f x = [x, f(x), f(f(x)), f(f(f(f))), ...]
+-}
+
+-- Возвращает бесконечный список ВВП на годы и годы вперёд
+-- yearGDP 100 0.1 ~> [100, 100.1, 100.20009(9), 100.3003.., ...]
+yearGDP :: Double -> Double -> [Double]
+yearGDP now percent = error "not implemented"
+
+-- Возвращает количество лет, которые нужны Китаю, чтобы догнать США в текущих условиях
+inHowManyYearsChinaWins :: Int
+inHowManyYearsChinaWins = error "not implemented"
+
+{-
+  Пусть у нас есть некоторая лента событий, каждое сообщение в которой говорит,
+  сколько людей заболело в очередной стране. Нужно посчитать, сколько больных
+  в каждой из перечисленных стран:
+
+    stat [(China, 1000), (Italy, 47), (Russia, 14), (Italy, 98), (China, 107)] ~>
+      [(China, 1107), (Russia, 14), (Italy, 145), (USA, 0), (GreatBritain, 0)]
+
+    stat [(China, 80026)] ~>
+      [(China, 80026), (Russia, 0), (Italy, 0), (USA, 0), (GreatBritain, 0)]
+
+    В тестах есть случай с большими списками. Поэтому обычное решение будет
+    неэффективным и нужно придумать как его улучшить с помощью строгих вычислений.
+-}
+
+data Country = Country String Integer deriving (Eq, Show)
+
+allCountries :: [Country]
+allCountries =
+  [ Country "China" 0
+  , Country "Russia" 0
+  , Country "Italy" 0
+  , Country "USA" 0
+  , Country "GreatBritain" 0 ]
+
+stat :: [Country] -> [Country]
+stat events = error "not implemented"
+
+-- </Задачи для самостоятельного решения>
---
+++ test/Lecture05Spec.hs
@@ -0,0 +1,61 @@
+module Lecture05Spec where
+
+import Test.Hspec
+import System.CPUTime
+
+import Lecture05
+
+gen :: Country -> [Country]
+gen c@(Country n k) =
+  let nextCountry = Country n (toInteger (length n) + k + 1)
+  in c : gen nextCountry
+
+spec :: Spec
+spec = do
+  describe "nthPrime" $ do
+    it "nthPrime 1 ~> 2" $
+      nthPrime (3 :: Int) `shouldBe` (5 :: Integer)
+    it "nthPrime 3 ~> 5" $
+      nthPrime (1 :: Int) `shouldBe` (2 :: Integer)
+    it "nthPrime 983 ~> 7753" $
+      nthPrime (983 :: Int) `shouldBe` (7753 :: Integer)
+  describe "yearGDP" $ do
+    it "yearGDP 100 0.1 ~> [100, 100.1, 100.20009(9), 100.3003.., ...]" $
+      (take 4 $ yearGDP 100 0.1) `shouldBe` ([100.0,100.1,100.20009999999998,100.30030009999997] :: [Double])
+    it "yearGDP 130 10 ~> [130.0,143.0,157.3, ...]" $
+      (take 3 $ yearGDP 130 10) `shouldBe` ([130.0,143.0,157.3] :: [Double])
+  describe "stat" $ do
+    it "stat [China 80026] ~> [China 80026, Russia 0, Italy 0, USA 0, GreatBritain 0]" $
+      stat [Country "China" 80026] `shouldBe` [(Country "China" 80026), (Country "Russia" 0), (Country "Italy" 0), (Country "USA" 0), (Country "GreatBritain" 0)]
+    it "stat [China 1000, Italy 47, (Russia 14), (Italy 98), (China 107)] ~> [(China, 1107), (Russia 14), (Italy 145), (USA 0), (GreatBritain 0)]" $
+      stat [(Country "China" 1000), (Country "Italy" 47), (Country "Russia" 14), (Country "Italy" 98), (Country "China" 107)] `shouldBe` [(Country "China" 1107), (Country "Russia" 14), (Country "Italy" 145), (Country "USA" 0), (Country "GreatBritain" 0)]
+    it "someBigList ~> [(China, 132299370000), (Russia 1263433500), (Italy 2997000), (USA 4497000), (GreatBritain 4062499837500000)]" $ do
+      let
+        china  = take 210000 $ gen (Country "China" 0)
+        russia = take 19000 $ gen (Country "Russia" 0)
+        italy  = take 1000 $ gen (Country "Italy" 0)
+        usa    = take 1500 $ gen (Country "USA" 0)
+        uk     = take 25000000 $ gen (Country "GreatBritain" 0)
+      start <- getCPUTime
+      isCorrectResults <- pure $! (==)
+        (stat (china ++ russia ++ italy ++ usa ++ uk))
+        [ (Country "China" 132299370000)
+        , (Country "Russia" 1263433500)
+        , (Country "Italy" 2997000)
+        , (Country "USA" 4497000)
+        , (Country "GreatBritain" 4062499837500000)]
+      end <- getCPUTime
+      let
+        okayExecTime = 20
+        execTime = (fromIntegral (end - start) `div` 1000000000000) :: Integer
+      putStrLn $ "Exec time: " ++ show execTime ++ " seconds\n"
+      if execTime < okayExecTime then pure () else do
+        putStrLn "!!!!!!!!!!!!\n"
+        putStrLn "Function's execution time is too big\n"
+        putStrLn $ "Exec time: " ++ show execTime ++ " seconds, should be less than: " ++ show okayExecTime ++ " seconds\n"
+        putStrLn "!!!!!!!!!!!!\n"
+      if isCorrectResults then pure () else do
+        putStrLn "!!!!!!!!!!!!\n"
+        putStrLn "Results are incorrect\n"
+        putStrLn "!!!!!!!!!!!!\n"
+      isCorrectResults `shouldSatisfy` \k -> k && execTime < okayExecTime
